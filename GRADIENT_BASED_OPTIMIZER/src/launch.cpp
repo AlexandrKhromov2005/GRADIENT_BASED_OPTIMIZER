7a528a2a4f500602c2a3f9bd0e4c97754be4f723
AlexandrKhromov2005 <alalkhromov@edu.hse.ru>
2025-02-18 10:52:37 +0300

    make launching compact and add writing results to txt

--- GRADIENT_BASED_OPTIMIZER/src/launch.cpp
+++ GRADIENT_BASED_OPTIMIZER/src/launch.cpp
@@ -106,7 +106,7 @@ cv::Mat get_wm(const cv::Mat& cv_image) {
 }
 
 
-void launch(const std::string& image, const std::string& new_image, const std::string& wm, const std::string& new_wm) {
+/*void launch(const std::string& image, const std::string& new_image, const std::string& wm, const std::string& new_wm) {
 	double mse_total = 0, psnr_total = 0, ncc_total = 0, ber_total = 0, ssim_total = 0;
 	std::vector<cv::Mat> embeded_images;
 	cv::Mat cv_image = readImage(image);
@@ -603,6 +603,109 @@ void launch(const std::string& image, const std::string& new_image, const std::s
 	std::cout << "Average NCC: " << ncc_avg << std::endl;
 	std::cout << "Average BER: " << ber_avg << std::endl;
 	std::cout << "Average SSIM: " << ssim_avg << std::endl;
+}*/
+
+using AttackFunction = std::function<cv::Mat(const cv::Mat&)>;
+using MetricCalculator = std::function<double(const cv::Mat&, const cv::Mat&)>;
+
+struct AttackConfig {
+	std::string name;
+	AttackFunction attack;
+	bool use_cropped_comparison = false;
+};
+
+std::string getFileNameWithoutExtension(const std::string& path) {
+	size_t lastSlashPos = path.find_last_of('/');
+	size_t dotPos = path.find_last_of('.');
+
+	if (lastSlashPos != std::string::npos && dotPos != std::string::npos) {
+		return path.substr(lastSlashPos + 1, dotPos - lastSlashPos - 1);
+	}
+
+	return "";
+}
+
+void processAttack(
+	const std::vector<cv::Mat>& embeded_images,
+	const cv::Mat& cv_image,
+	const cv::Mat& cv_wm,
+	const AttackConfig& config,
+	MetricCalculator metric,
+	int iterations = 10,
+	const std::string& output_file = "")
+{
+	double mse_total = 0, psnr_total = 0, ncc_total = 0, ber_total = 0, ssim_total = 0;
+
+	std::ofstream output(output_file, std::ios::app);
+	if (!output.is_open()) {
+		std::cerr << "Error opening file: " << output_file << std::endl;
+		return;
+	}
+
+	output << "Attack: " << config.name << std::endl;
+
+	for (size_t i = 0; i < iterations; ++i) {
+		cv::Mat img = config.attack(embeded_images[i].clone());
+		cv::Mat original = config.use_cropped_comparison ?
+			config.attack(cv_image.clone()) : cv_image.clone();
+
+		cv::Mat wm = get_wm(img);
+
+		mse_total += metric(original, img);
+		psnr_total += computePSNR(original, img);
+		ncc_total += computeNCC(original, img);
+		ber_total += computeBER(cv_wm, wm);
+		ssim_total += computeSSIM(original, img);
+	}
+
+	output << "Average MSE: " << mse_total / iterations << std::endl
+		<< "Average PSNR: " << psnr_total / iterations << std::endl
+		<< "Average NCC: " << ncc_total / iterations << std::endl
+		<< "Average BER: " << ber_total / iterations << std::endl
+		<< "Average SSIM: " << ssim_total / iterations << std::endl
+		<< std::endl;
+
+	output.close();
+}
+
+void launch(const std::string& image, const std::string& new_image,const std::string& wm, const std::string& new_wm){
+	std::vector<cv::Mat> embeded_images;
+	cv::Mat cv_image = readImage(image);
+	cv::Mat cv_wm = readImage(wm);
+
+	for (size_t i = 0; i < 10; ++i) {
+		embend_wm(image, new_image, wm);
+		get_wm(new_image, new_wm);
+		embeded_images.push_back(readImage(new_image));
+	}
+
+	std::vector<AttackConfig> attacks = {
+		{"No attack", [](const cv::Mat& img) { return img; }},
+		{"Brightness increase", [](const cv::Mat& img) { return brightnessIncrease(img, 10); }},
+		{"Brightness decrease", [](const cv::Mat& img) { return brightnessDecrease(img, 10); }},
+		{"Contrast increase", [](const cv::Mat& img) { return contrastIncrease(img, 1.5); }},
+		{"Contrast decrease", [](const cv::Mat& img) { return contrastDecrease(img, 0.5); }},
+		{"Salt Pepper Noise", [](const cv::Mat& img) { return saltPepperNoise(img, 0.05); }},
+		{"Speckle Noise", [](const cv::Mat& img) { return speckleNoise(img, 0.05); }},
+		{"Histogram Equalization", [](const cv::Mat& img) { return histogramEqualization(img); }},
+		{"Sharpening", [](const cv::Mat& img) { return sharpening(img); }},
+		{"JPEG Compression (QF=90)", [](const cv::Mat& img) { return jpegCompression(img, 90); }},
+		{"JPEG Compression (QF=80)", [](const cv::Mat& img) { return jpegCompression(img, 80); }},
+		{"JPEG Compression (QF=70)", [](const cv::Mat& img) { return jpegCompression(img, 70); }},
+		{"Gaussian Filtering", [](const cv::Mat& img) { return gaussianFiltering(img, 5); }},
+		{"Median Filtering", [](const cv::Mat& img) { return medianFiltering(img, 5); }},
+		{"Average Filtering", [](const cv::Mat& img) { return averageFiltering(img, 5); }},
+		{"Cropping from Corner", [](const cv::Mat& img) { return cropFromCorner(img, 100); }, true},
+		{"Cropping from Center", [](const cv::Mat& img) { return cropFromCenter(img, 100); }, true},
+		{"Cropping from Edge", [](const cv::Mat& img) { return cropFromEdge(img, 100); }, true}
+	};
+
+	std::string result_filename = "results_" + getFileNameWithoutExtension(image) + ".txt";
+
+	for (const auto& attack : attacks) {
+		MetricCalculator metric = attack.use_cropped_comparison ? computeMSE : computeMSE; // ѕример, метрика должна быть передана сюда
+		processAttack(embeded_images, cv_image, cv_wm, attack, metric, 10, result_filename);
+	}
 }
 
 
