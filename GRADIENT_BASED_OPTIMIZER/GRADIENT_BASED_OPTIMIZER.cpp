c3f31799e2a34760983381c6cac334cffbc3a0c2
AlexandrKhromov2005 <alalkhromov@edu.hse.ru>
2025-03-29 20:10:00 +0300

    add timer

--- GRADIENT_BASED_OPTIMIZER/GRADIENT_BASED_OPTIMIZER.cpp
+++ GRADIENT_BASED_OPTIMIZER/GRADIENT_BASED_OPTIMIZER.cpp
@@ -1,10 +1,14 @@
 ﻿#include <iostream>
+#include <chrono>
 #include <opencv2/opencv.hpp>
 #include "src/launch.h"
 
 int main()
 {
-    std::vector<std::string> names = { "airplane", "baboon", "boat", "bridge", "earth_from_space", "lake", "lenna", "pepper" };
+    auto start = std::chrono::high_resolution_clock::now();
+
+    std::vector<std::string> names = { "airplane", "baboon", "boat", "bridge",
+                                      "earth_from_space", "lake", "lenna", "pepper" };
 
     for (std::string name : names) {
         std::cout << name << " is started" << std::endl;
@@ -19,4 +23,27 @@ int main()
         std::cout << name << " is finished" << std::endl;
     }
 
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = end - start;
+
+    auto hours = std::chrono::duration_cast<std::chrono::hours>(duration);
+    duration -= hours;
+    auto minutes = std::chrono::duration_cast<std::chrono::minutes>(duration);
+    duration -= minutes;
+    auto seconds = std::chrono::duration_cast<std::chrono::seconds>(duration);
+
+    std::cout << "\nTotal execution time: ";
+    bool need_space = false;
+
+    if (hours.count() > 0) {
+        std::cout << hours.count() << "h ";
+        need_space = true;
+    }
+    if (minutes.count() > 0 || need_space) {
+        std::cout << minutes.count() << "m ";
+        need_space = true;
+    }
+    std::cout << seconds.count() << "s" << std::endl;
+
+    return 0;
 }
