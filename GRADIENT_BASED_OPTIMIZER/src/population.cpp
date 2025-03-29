136f474ae9bb4740c262287eeec97395158cf17a
AlexandrKhromov2005 <alalkhromov@edu.hse.ru>
2025-03-29 19:57:59 +0300

    rewrite with emulating random compression for each block

--- GRADIENT_BASED_OPTIMIZER/src/population.cpp
+++ GRADIENT_BASED_OPTIMIZER/src/population.cpp
@@ -26,9 +26,6 @@ cv::Mat Population::apply_vec(const cv::Mat& block, std::array<double, VEC_SIZE>
 
     cv::Mat new_block = block.clone();
 
-    //std::ostringstream debugStream;
-    //debugStream << "apply_vec: ";
-
     for (size_t i = 0; i < VEC_SIZE; ++i) {
         int row = static_cast<int>(ZONE0[i].first);
         int col = static_cast<int>(ZONE0[i].second);
@@ -36,63 +33,12 @@ cv::Mat Population::apply_vec(const cv::Mat& block, std::array<double, VEC_SIZE>
         double original_val = block.at<double>(row, col);
         double computed_val = SIGN(original_val) * std::fabs(std::fabs(original_val) + vec[i]);
         new_block.at<double>(row, col) = computed_val;
-
-
-        //debugStream << "[" << row << "," << col << "] " << original_val << "->" << computed_val << "  ";
     }
-    //std::cout << debugStream.str() << std::endl;
-    //cv::Mat test1, test2, test3, test4;
-    //cv::idct(block, test1);
-    //cv::idct(new_block, test2);
-    //test1.convertTo(test3, CV_8U);
-    //test2.convertTo(test4, CV_8U);
-    //double psnr = calculatePSNR(test3, test4);
-    //std::cout << "app psnr: " << psnr << std::endl;
 
     return new_block;
 }
 
-cv::Mat Population::emulateJPEG70(const cv::Mat& block) {
-    const int quality = 70;
-    static const std::array<std::array<int, 8>, 8> std_quantization_matrix = { {
-        {16, 11, 10, 16, 24, 40, 51, 61},
-        {12, 12, 14, 19, 26, 58, 60, 55},
-        {14, 13, 16, 24, 40, 57, 69, 56},
-        {14, 17, 22, 29, 51, 87, 80, 62},
-        {18, 22, 37, 56, 68, 109, 103, 77},
-        {24, 35, 55, 64, 81, 104, 113, 92},
-        {49, 64, 78, 87, 103, 121, 120, 101},
-        {72, 92, 95, 98, 112, 100, 103, 99}
-    } };
 
-    double scale_factor = (quality < 50) ? 5000.0 / quality : 200.0 - 2.0 * quality;
-
-    std::array<std::array<int, 8>, 8> quantization_matrix;
-    for (int i = 0; i < 8; ++i) {
-        for (int j = 0; j < 8; ++j) {
-            int q = static_cast<int>(std_quantization_matrix[i][j] * scale_factor / 100.0);
-            quantization_matrix[i][j] = (q > 0) ? q : 1;
-        }
-    }
-
-    cv::Mat blockDouble;
-    block.convertTo(blockDouble, CV_64F);
-    cv::Mat dctBlock;
-    cv::dct(blockDouble, dctBlock);
-
-    for (int i = 0; i < 8; ++i) {
-        for (int j = 0; j < 8; ++j) {
-            dctBlock.at<double>(i, j) = std::round(dctBlock.at<double>(i, j) / quantization_matrix[i][j]) * quantization_matrix[i][j];
-        }
-    }
-
-    cv::Mat idctBlock;
-    cv::idct(dctBlock, idctBlock);
-    cv::Mat result;
-    idctBlock.convertTo(result, CV_8U);
-
-    return result;
-}
 
 double Population::calculateOf(const cv::Mat& block, const std::array<double, VEC_SIZE>& vec, uchar bit) {
     cv::Mat blockDouble;
@@ -106,10 +52,10 @@ double Population::calculateOf(const cv::Mat& block, const std::array<double, VE
     cv::Mat newblock;
     newblockDouble.convertTo(newblock, CV_8U);
 
-    // Применяем атаку JPEG70
-    cv::Mat attackedBlock = emulateJPEG70(newblock);
+    // emulate random JPEG-attack
+    int quality = rand_int_1_to_100();
+    cv::Mat attackedBlock = compress_block(newblock, quality);
 
-    // Вычисляем DCT атакованного блока
     cv::Mat attackedBlockDouble;
     attackedBlock.convertTo(attackedBlockDouble, CV_64F);
     cv::Mat attackedDCTblock;
