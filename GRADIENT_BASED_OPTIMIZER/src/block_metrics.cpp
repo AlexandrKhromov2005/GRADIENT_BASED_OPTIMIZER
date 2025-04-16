9dbfccf741337345a72ee31ebcd91ad002017a7b
Bio_GD <tkismailov@edu.hse.ru>
2025-04-16 13:36:30 +0300

    fix jpeg70

--- GRADIENT_BASED_OPTIMIZER/src/block_metrics.cpp
+++ GRADIENT_BASED_OPTIMIZER/src/block_metrics.cpp
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <limits>
 #include "block_metrics.h"
+#include "config.h"
 
 double calculateMSE(const cv::Mat& block1, const cv::Mat& block2) {
     double mse = 0.0;
@@ -30,7 +31,7 @@ double calculatePSNR(const cv::Mat& block1, const cv::Mat& block2) {
 }
 
 double calc_s_zero(const cv::Mat& block) {
-    constexpr std::array<std::pair<std::size_t, std::size_t>, 11> REG0 = { {
+    constexpr std::array<std::pair<std::size_t, std::size_t>, VEC_SIZE / 2> REG0 = { {
         {7, 1}, {6, 1}, {5, 1},
         {5, 3}, {4, 3}, {3, 3},
         {3, 5}, {2, 5}, {1, 5},
@@ -49,7 +50,7 @@ double calc_s_zero(const cv::Mat& block) {
 }
 
 double calc_s_one(const cv::Mat& block) {
-    constexpr std::array<std::pair<std::size_t, std::size_t>, 11> REG1 = { {
+    constexpr std::array<std::pair<std::size_t, std::size_t>, VEC_SIZE / 2> REG1 = { {
         {7, 0}, {6, 0},
         {6, 2}, {5, 2}, {4, 2},
         {4, 4}, {3, 4}, {2, 4},
